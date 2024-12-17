4a5cde32688bb479b22d2d1cc26ccd2e600b87d4
Luiz Fernando <luizfsoliveira.lm@gmail.com>
2024-12-16 21:05:55 -0300

    adicionando o test

---
+++ test_main.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_ola_mundo():
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_ola_mundo_json():
+    response = client.get("/")
+    assert response.json() == {"ola":"Mundo"}
